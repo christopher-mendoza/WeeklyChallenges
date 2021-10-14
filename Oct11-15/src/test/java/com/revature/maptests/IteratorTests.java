3489a6342b7a563ac5a07431d4ab7bdfd4a8185b
christopher-mendoza <chesmend@gmail.com>
2021-10-14 15:46:55 -0700

    added hashmap traversal test

--- Oct11-15/src/test/java/com/revature/maptests/IteratorTests.java
+++ Oct11-15/src/test/java/com/revature/maptests/IteratorTests.java
@@ -1,5 +1,26 @@
 package com.revature.maptests;
 
-public class IteratorTests {
+import java.util.HashMap;
+import MapIterator.Iterator;
 
+import org.junit.Test;
+
+public class IteratorTests {
+	HashMap<Integer, String> map = new HashMap<Integer, String>();
+	
+	Iterator I = new Iterator();
+	
+	@Test
+	public void traverseTest() {
+		map.put(4, "my");
+		map.put(1, "Hi,");
+		map.put(3, "is");
+		map.put(2, "this");
+		map.put(5, "hashmap");
+		map.put(7, "function");
+		map.put(6, "traversal");
+		
+		I.traverse(map);
+	}
+	
 }
